ae7a01fd206c5633cb725a273d3d815142b457a6
sabeer <mohammedsabeer.kt@gmail.com>
2024-01-17 14:51:42 +0530

    OTPimg

--- lib/widgets/otp.dart
+++ lib/widgets/otp.dart
@@ -3,6 +3,7 @@ import 'package:chats/widgets/textform.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class PhoneSignIn extends StatefulWidget {
   const PhoneSignIn({super.key});
@@ -18,24 +19,38 @@ class _PhoneSignInState extends State<PhoneSignIn> {
   }
 
   final TextEditingController phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-      ),
+      appBar: AppBar(),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          TextField(
-             controller:phoneController ,
-             
+          // Add Image widget here
+          Lottie.asset(
+            'assets/otp.json', // Replace with the actual path or asset name
           ),
-        
+          SizedBox(height: 20), // Add some space between image and text field
+          // Text field goes below the image
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: phoneController,
+              decoration: InputDecoration(
+                labelText: 'Enter your phone number',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          SizedBox(height: 20), // Add some space between text field and button
           ElevatedButton(
-              onPressed: () {
-                phoneSignIn();
-              },
-              child: Text("Sent OTP"))
+            onPressed: () {
+              phoneSignIn();
+            },
+            child: Text("Sent OTP"),
+          ),
         ],
       ),
     );
